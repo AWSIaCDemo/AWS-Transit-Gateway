3e8f8fa7c5346ff065626f297205934ffbd4bb6c
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-08-17 16:36:14 -0400

    Update c13-01-TGW.tf

--- terraform-manifests/c13-01-TGW.tf
+++ terraform-manifests/c13-01-TGW.tf
@@ -1,5 +1,4 @@
-resource "aws_ec2_transit_gateway" "NW-TransitGtwy-USWest1" {
-  name                            ="Master-TGW-001"
+resource "aws_ec2_transit_gateway" "Master-TGW-001" {
   amazon_side_asn                 = 64512
   description                     = "Core-TransitGtwy"
   auto_accept_shared_attachments  = "disable"
