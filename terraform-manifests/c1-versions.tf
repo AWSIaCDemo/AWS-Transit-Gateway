75afe5e5fbbde0e53aec47ece4aab97931f1290d
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-08-17 17:46:44 -0400

    Update c1-versions.tf

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.4"
+      version = "~> 3.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "~> 4.4"
+      version = "~> 3.0
     }    
     random = {
       source = "hashicorp/random"
-      version = "~> 4.4"
+      version = "~> 3.0
     }            
   }
   # Adding Backend as S3 for Remote State Storage
